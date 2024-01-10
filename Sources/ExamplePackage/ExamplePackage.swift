8e7a52c9c3cbc168d863d2c3f967772f433a54b5
luis purizaga <backup.user.leps@outlook.com>
2024-01-10 09:08:17 -0500

    example to pm

--- Sources/ExamplePackage/ExamplePackage.swift
+++ Sources/ExamplePackage/ExamplePackage.swift
@@ -1,2 +1,26 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import SwiftUI
+
+public struct LinearGradientBackground: ViewModifier {
+    let gradient: LinearGradient
+    let opacity: Double
+    public func body(content: Content) -> some View {
+        content.background(
+            gradient
+                .opacity(opacity)
+                .edgesIgnoringSafeArea(.all)
+        )
+    }
+}
+
+extension View {
+/// Generic background function for scenes
+    func linearGradientBackground(_ gradient: LinearGradient = LinearGradient(colors: [.red, .pink], startPoint: .top, endPoint: .bottom), _ opacity: Double = 1.0) -> some View {
+        self
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            .modifier(LinearGradientBackground(gradient: gradient, opacity: opacity))
+    }
+}
